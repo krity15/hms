3d879113689630e7e4ce592d7431a9a0f1fba3f6
Kriti Ghimire <98736181+krity15@users.noreply.github.com>
2022-08-03 19:58:17 +0545

    Add files via upload

---
+++ myhmsdb.sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 16, 2020 at 02:34 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myhmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admintb`
+--
+
+CREATE TABLE `admintb` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admintb`
+--
+
+INSERT INTO `admintb` (`username`, `password`) VALUES
+('admin', 'admin123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointmenttb`
+--
+
+CREATE TABLE `appointmenttb` (
+  `pid` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `doctor` varchar(30) NOT NULL,
+  `docFees` int(5) NOT NULL,
+  `appdate` date NOT NULL,
+  `apptime` time NOT NULL,
+  `userStatus` int(5) NOT NULL,
+  `doctorStatus` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointmenttb`
+--
+
+INSERT INTO `appointmenttb` (`pid`, `ID`, `fname`, `lname`, `gender`, `email`, `contact`, `doctor`, `docFees`, `appdate`, `apptime`, `userStatus`, `doctorStatus`) VALUES
+
+
+(9, 13, 'William', 'Blake', 'Male', 'william@gmail.com', '8683619153', 'Tiwary', 450, '2020-03-26', '14:00:00', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `name` varchar(30) NOT NULL,
+  `email` text NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `message` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
+
+('Jane', 'jane@gmail.com', '7869869757', 'Great service!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctb`
+--
+
+CREATE TABLE `doctb` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `spec` varchar(50) NOT NULL,
+  `docFees` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctb`
+--
+
+INSERT INTO `doctb` (`username`, `password`, `email`, `spec`, `docFees`) VALUES
+
+('Abbis', 'abbis123', 'abbis@gmail.com', 'Neurologist', 1500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patreg`
+--
+
+CREATE TABLE `patreg` (
+  `pid` int(11) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `cpassword` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patreg`
+--
+
+INSERT INTO `patreg` (`pid`, `fname`, `lname`, `gender`, `email`, `contact`, `password`, `cpassword`) VALUES
+(1, 'Ram', 'Kumar', 'Male', 'ram@gmail.com', '9876543210', 'ram123', 'ram123'),
+(2, 'Nancy', 'Deborah', 'Female', 'nancy@gmail.com', '9128972454', 'nancy123', 'nancy123'),
+(3, 'Kenny', 'Sebastian', 'Male', 'kenny@gmail.com', '9809879868', 'kenny123', 'kenny123'),
+(4, 'William', 'Blake', 'Male', 'william@gmail.com', '8683619153', 'william123', 'william123'),
+(5, 'Peter', 'Norvig', 'Male', 'peter@gmail.com', '9609362815', 'peter123', 'peter123');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prestb`
+--
+
+CREATE TABLE `prestb` (
+  `doctor` varchar(50) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `appdate` date NOT NULL,
+  `apptime` time NOT NULL,
+  `disease` varchar(250) NOT NULL,
+  `allergy` varchar(250) NOT NULL,
+  `prescription` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `prestb`
+--
+
+INSERT INTO `prestb` (`doctor`, `pid`, `ID`, `fname`, `lname`, `appdate`, `apptime`, `disease`, `allergy`, `prescription`) VALUES
+('Ram', 4, 11, 'Sam', 'Shrestha', '2020-03-27', '15:00:00', 'Cough', 'Nothing', 'Just take a teaspoon of Benadryl every night');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointmenttb`
+--
+ALTER TABLE `appointmenttb`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `patreg`
+--
+ALTER TABLE `patreg`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointmenttb`
+--
+ALTER TABLE `appointmenttb`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `patreg`
+--
+ALTER TABLE `patreg`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+CREATE TABLE `transaction` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `cust_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `cust_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `card_number` bigint(20) NOT NULL,
+ `card_cvc` int(5) NOT NULL,
+ `card_exp_month` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
+ `card_exp_year` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+ `item_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `item_number` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+ `item_price` float(10,2) NOT NULL,
+ `item_price_currency` varchar(10) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'usd',
+ `paid_amount` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+ `paid_amount_currency` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+ `txn_id` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `payment_status` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+ `created` datetime NOT NULL,
+ `modified` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
